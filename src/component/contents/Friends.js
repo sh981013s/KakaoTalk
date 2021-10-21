2b6a427bbd5841fd5fb759010e9635e9f14d1d29
SeungHwan-Lee <sh981013s@gmail.com>
2021-10-22 03:15:30 +0900

    Feat: birthday modal

--- src/component/contents/Friends.js
+++ src/component/contents/Friends.js
@@ -2,12 +2,13 @@ import { useEffect, useState } from 'react';
 import jason from '../../resources/img/jason.jpg';
 import basic from '../../resources/img/basic_profile.jpg';
 import { getData } from '../../utils/Api';
-import { Input, Menu, Popover, Select } from 'antd';
+import { Popover } from 'antd';
 
 function Friends(props) {
 	const [copyList, setCopyList] = useState([]);
-
 	const [showProfile, setShowProfile] = useState(true);
+	const [birthDayFriends, setBirthDayFriends] = useState([[],[],[]]);
+	const [bdLastPersonSave, setBdLastPersonSave] = useState([]);
 
 	const getFriendsData = async () => {
 		const resultData = await getData.get('member/getFriends');
@@ -19,11 +20,11 @@ function Friends(props) {
 	}, []);
 
 	const bowl = [
-		{ name: 'robot1', desc: 'hi', pic: 'jason' },
-		{ name: 'robot2', desc: 'lol', pic: 'jason' },
-		{ name: 'robot3', desc: '', pic: 'jason' },
-		{ name: 'robot4', desc: 'kk', pic: 'jason' },
-		{ name: 'robot5', desc: 'good', pic: 'jason' },
+		{ name: 'robot1', desc: 'hi', pic: 'jason', bd: 19981023 },
+		{ name: 'robot2', desc: 'lol', pic: 'jason', bd: 19981021  },
+		{ name: 'robot3', desc: '', pic: 'jason', bd: 19981022  },
+		{ name: 'robot4', desc: 'kk', pic: 'jason', bd: 19981021  },
+		{ name: 'robot5', desc: 'good', pic: 'jason', bd: 19981030  },
 	];
 
 	useEffect(() => {
@@ -41,6 +42,135 @@ function Friends(props) {
 	useEffect(() => {
 		props.func(copyList);
 	}, [copyList]);
+
+	const getMonthlyBirthDay = () => {
+
+		const today = new Date();
+		const year = today.getFullYear();
+		const month = today.getMonth();
+		const day = today.getDate();
+		let minDateNum = today.getDate() - 15;
+		let minDateToUse = new Date(year, month, day);
+		minDateToUse.setDate(minDateNum);
+
+		const getMonthCorrectly = date => {
+			if((date.getMonth() +1) < 10 ) {
+				const tmp = (date.getMonth() +1).toString();
+				return `0${tmp}`;
+			} else {
+				const tmp = (date.getMonth() +1).toString();
+				return `${tmp}`;
+			}
+		}
+
+		const getDayCorrectly = date => {
+			if((date.getDate()) < 10 ) {
+				const tmp = (date.getDate()).toString();
+				return `0${tmp}`;
+			} else {
+				const tmp = (date.getDate()).toString();
+				return `${tmp}`;
+			}
+		}
+
+		const todayDateNum = parseInt(today.getFullYear().toString() + getMonthCorrectly(today).toString() + getDayCorrectly(today).toString());
+		minDateToUse = parseInt(minDateToUse.getFullYear().toString() + getMonthCorrectly(minDateToUse).toString() + getDayCorrectly(minDateToUse).toString());
+
+
+
+		const bdPast = copyList.filter(user => {
+			return  parseInt(user.bd.toString().slice(-4)) >= parseInt(minDateToUse.toString().slice(-4)) && parseInt(user.bd.toString().slice(-4)) < parseInt(todayDateNum.toString().slice(-4))
+		})
+
+		const bdToday = copyList.filter(user => {
+			return  parseInt(user.bd.toString().slice(-4)) === parseInt(todayDateNum.toString().slice(-4))
+		})
+
+		const bdUpcoming = copyList.filter(user => {
+			return  parseInt(user.bd.toString().slice(-4)) > parseInt(todayDateNum.toString().slice(-4))
+		})
+
+		setBirthDayFriends([[...bdPast],[...bdToday],[...bdUpcoming]])
+
+	}
+
+	let last = {bd:1};
+
+	const content = (
+		<div class='friendsModal'>
+
+			<h2>Friends with Birthdays</h2>
+			<div className='friendsModalRow'>
+				<h3>Past Birthdays</h3>
+			{
+				birthDayFriends[0].map((user) => {
+					if(user.bd !== last.bd) {
+						last = user;
+						console.log(last);
+						return(
+							<>
+								<div>{user.bd}</div>
+								<div>{user.name}</div>
+							</>
+						)
+					} else {
+						last = user;
+						return(
+							<div>{user.name}</div>
+						)
+					}
+				})
+			}
+			<hr/>
+			</div>
+			<div className='friendsModalRow'>
+				<h3>Today</h3>
+				{
+					birthDayFriends[1].map((user) => {
+						if(user.bd !== last.bd) {
+							last = user;
+							console.log(last);
+							return(
+								<>
+									<div>{user.bd}</div>
+									<div>{user.name}</div>
+								</>
+							)
+						} else {
+							last = user;
+							return(
+								<div>{user.name}</div>
+							)
+						}
+					})
+				}
+				<hr/>
+			</div>
+			<div className='friendsModalRow'>
+				<h3>Upcoming Birthdays</h3>
+				{
+					birthDayFriends[2].map((user) => {
+						if(user.bd !== last.bd) {
+							last = user;
+							console.log(last);
+							return(
+								<>
+									<div>{user.bd}</div>
+									<div>{user.name}</div>
+								</>
+							)
+						} else {
+							last = user;
+							return(
+								<div>{user.name}</div>
+							)
+						}
+					})
+				}
+			</div>
+
+		</div>
+	)
 
 	return (
 		<main className="friendsList">
@@ -61,6 +191,23 @@ function Friends(props) {
 							</div>
 						</div>
 					</div>
+					<hr />
+					<p>Friends with Birthdays {copyList.length}</p>
+					<Popover placement='left' overlayClassName='birthDay' content={content} trigger='click' onClick={getMonthlyBirthDay}>
+						{/*<Button onClick={()=>setTmp(!tmp)}>Click me</Button>*/}
+						<div className='userComponent'>
+							<div className='friendsListIcon'>
+								<div className='iconBox'>
+									<i className="fas fa-birthday-cake fa-3x" />
+								</div>
+
+							</div>
+							<div className='userComponentDetails'>
+								<div className="userComponentName"><h4>View more birthdays</h4></div>
+							</div>
+
+						</div>
+					</Popover>
 					<hr />
 					<p>friends {copyList.length}</p>
 				</>

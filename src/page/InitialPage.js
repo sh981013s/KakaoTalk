c6fefc104626555d7bb37873704e630da9160b5b
SeungHwan-Lee <sh981013s@gmail.com>
2021-10-29 00:31:44 +0900

    kakao initial page layout & styles added

--- src/page/InitialPage.js
+++ src/page/InitialPage.js
@@ -1,6 +1,29 @@
+import { Input, Checkbox } from 'antd';
+import mainImg from '../resources/img/kakaoLoginMain.png';
+
 const InitialPage = () => (
-  <div>
-    <p>asdasd</p>
+  <div className='loginPage'>
+    <div className='yellowBox'>
+      <div className='loginMain'>
+        <div className='loginImage'>
+          <img src={mainImg} alt='lol' />
+        </div>
+        <div className='inputBox'>
+          <Input placeholder='Email or Phone Number' />
+          <Input placeholder='Password' />
+        </div>
+        <button className='login' type='submit'>
+          Login
+        </button>
+        <div className='desc'>
+          <Checkbox />
+          <p>Keep me logged in</p>
+        </div>
+        <div className='register'>
+          <p>Create a SeungKaoTalk Account</p>
+        </div>
+      </div>
+    </div>
   </div>
 );
 

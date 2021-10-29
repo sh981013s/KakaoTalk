e7f90934931f297c25d2bd465b5b8ab87713d81b
SeungHwan-Lee <sh981013s@gmail.com>
2021-10-30 01:24:31 +0900

    register 2nd page layout & styles added

--- src/page/register/Register.js
+++ src/page/register/Register.js
@@ -17,7 +17,7 @@ const Register = () => (
             <p>로그인하여 새로운 카카오계정을 만들 수 있어요.</p>
             <p>Daum 아이디로 카카오계정 가입</p>
           </div>
-          <ul>
+          <ul className='regiBottom'>
             <li>이용약관</li>
             <li>개인정보 처리방침</li>
             <li>운영정책</li>
@@ -25,7 +25,9 @@ const Register = () => (
             <li>공지사항</li>
             <li>한국어</li>
           </ul>
-          <p>Copyright SeungHwanLee. All rights reserved.</p>
+          <p className='regiBottomText'>
+            Copyright SeungHwanLee. All rights reserved.
+          </p>
         </div>
       </div>
     </div>
@@ -51,7 +53,83 @@ const Register = () => (
                 거부하시는 경우에도 서비스는 이용이 가능합니다.
               </p>
             </div>
+            <hr />
+            <div className='firstCheckbox'>
+              <Checkbox />
+              <p className='checkboxText'>만 14세 이상입니다.</p>
+            </div>
+            <div className='otherCheckbox'>
+              <Checkbox />
+              <p className='otherCheckboxText'>[필수] 카카오계정 약관</p>
+              <div className='checkboxIcon'>
+                <i className='fas fa-chevron-right' />
+              </div>
+            </div>
+            <div className='otherCheckbox'>
+              <Checkbox />
+              <p className='otherCheckboxText'>[필수] 카카오 통합서비스 약관</p>
+              <div className='checkboxIcon'>
+                <i className='fas fa-chevron-right' />
+              </div>
+            </div>
+            <div className='secondDesc'>
+              <p>
+                본 약관은 회사가 제공하는 카카오, Daum 서비스 등에 공통
+                적용되며, 본 약관에 동의함으로써 해당 서비스들을 별도 이용계약
+                체결 없이 이용할 수 있습니다.
+              </p>
+            </div>
+            <div className='otherCheckbox'>
+              <Checkbox />
+              <p className='otherCheckboxText'>
+                [선택] 카카오알림 채널 추가 및 광고메시지 수신
+              </p>
+              <div className='checkboxIcon'>
+                <i className='fas fa-chevron-right' />
+              </div>
+            </div>
+            <div className='otherCheckbox'>
+              <Checkbox />
+              <p className='otherCheckboxText'>
+                [필수] 개인정보 수집 및 이용 동의
+              </p>
+              <div className='checkboxIcon'>
+                <i className='fas fa-chevron-right' />
+              </div>
+            </div>
+            <div className='otherCheckbox'>
+              <Checkbox />
+              <p className='otherCheckboxText'>
+                [선택] 위치정보 수집 및 이용 동의
+              </p>
+              <div className='checkboxIcon'>
+                <i className='fas fa-chevron-right' />
+              </div>
+            </div>
+            <div className='otherCheckbox'>
+              <Checkbox />
+              <p className='otherCheckboxText'>
+                [선택] 프로필정보 추가 수집 동의
+              </p>
+              <div className='checkboxIcon'>
+                <i className='fas fa-chevron-right' />
+              </div>
+            </div>
+            <button type='submit' className='agreeBtn'>
+              동의
+            </button>
           </div>
+          <ul className='regiBottom'>
+            <li>이용약관</li>
+            <li>개인정보 처리방침</li>
+            <li>운영정책</li>
+            <li>고객센터</li>
+            <li>공지사항</li>
+            <li>한국어</li>
+          </ul>
+          <p className='regiBottomText'>
+            Copyright SeungHwanLee. All rights reserved.
+          </p>
         </div>
       </div>
     </div>

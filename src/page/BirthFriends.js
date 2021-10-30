89d85b9c30da8abf5c965a9a5cf078906f707e47
SeungHwan-Lee <sh981013s@gmail.com>
2021-10-30 21:54:27 +0900

    chatList layout & styles set

--- src/page/BirthFriends.js
+++ src/page/BirthFriends.js
@@ -91,14 +91,6 @@ const BirthFriends = () => {
 
   return (
     <div className='entireBirthday'>
-      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions */}
-      <p
-        onClick={() => {
-          console.log(':::::::');
-        }}
-      >
-        asdasdsa
-      </p>
       {/*      <Popover
         placement='left'
         overlayClassName='profileOverall'

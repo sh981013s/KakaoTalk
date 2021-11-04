7957a56aec88f558745d9dc49f236d353e4a46e4
SeungHwan-Lee <sh981013s@gmail.com>
2021-11-04 18:31:29 +0900

    more page layout && styles set

--- src/page/MorePage.js
+++ src/page/MorePage.js
@@ -1,7 +1,7 @@
-import styled from "styled-components";
 import '../resources/css/mainpage/MainPage.scss';
 import { useEffect, useState } from 'react';
 import { getData } from '../utils/Api';
+import walletIcon from '../resources/img/cardImg.png';
 
 
 const MorePage = () =>  {
@@ -21,51 +21,99 @@ const MorePage = () =>  {
 		getInfo();
 	},[])
 	return (
-		(
-			<div className='mainWrap'>
-				<div className='userCard'>
-					<CardTop>
-						<TopText>
-							<TopName>
-								<p>{me.name}</p>
-							</TopName>
-							<TopEmail>
-								<p>{me.email}</p>
-							</TopEmail>
-						</TopText>
-					</CardTop>
-					<CardBtm>
-						<BtmText>
-							<p>My Subscription</p>
-							<p>icon</p>
-						</BtmText>
-					</CardBtm>
+		<div className='mainWrap'>
+			<div className='userCard'>
+				<div className='cardTop'>
+					<div className='cardTopDesc'>
+						<div className='cardTopDescText'>
+							<h2>{me.name}</h2>
+							<p>{me.email}</p>
+						</div>
+					</div>
+					<div className='cardTopDescIcon'>
+						<img src={walletIcon} alt='WalletIcon' />
+					</div>
+				</div>
+				<hr/>
+				<div className='cardBtm'>
+					<p>My Subscription</p>
+					<i className="fas fa-chevron-right" />
 				</div>
 			</div>
-		)
+			<ul className='moreIconBox'>
+				<li>
+					<div className='iconSingleBox'>
+						<i className="far fa-envelope" />
+						<p>Mail</p>
+					</div>
+				</li>
+				<li>
+					<div className='iconSingleBox'>
+						<i className="far fa-calendar-check" />
+						<p>Calender</p>
+					</div>
+				</li>
+				<li>
+					<div className='iconSingleBox'>
+						<i className="fas fa-inbox" />
+						<p>Talk Drive</p>
+					</div>
+				</li>
+				<li>
+					<div className='iconSingleBox'>
+						<i className="far fa-grin-wink" />
+						<p>Emoticon</p>
+					</div>
+				</li>
+				<li>
+					<div className='iconSingleBox'>
+						<i className="fas fa-tv" />
+						<p>TV</p>
+					</div>
+				</li>
+				<li>
+					<div className='iconSingleBox'>
+						<i className="fas fa-gifts" />
+						<p>Gift</p>
+					</div>
+				</li>
+				<li>
+					<div className='iconSingleBox'>
+						<i className="fas fa-bullhorn" />
+						<p>Notices</p>
+					</div>
+				</li>
+				<li>
+					<div className='iconSingleBox'>
+						<i className="fas fa-flask" />
+						<p>Kakao Lab</p>
+					</div>
+				</li>
+				<li>
+					<div className='iconSingleBox'>
+						<i className="fas fa-cog" />
+						<p>Settings</p>
+					</div>
+				</li>
+				<li>
+					<div className='iconSingleBox'>
+						<i className="far fa-question-circle" />
+						<p>Help</p>
+					</div>
+				</li>
+			</ul>
+			<hr/>
+			<div className='versionInfo'>
+				<i className="fas fa-info-circle" />
+				<p>KakaoTalk Info</p>
+				<p>ver. 2.9.6</p>
+			</div>
+		</div>
 	)
 
 }
 
-const CardTop = styled.div`
-	width: 100%;
-	height: 80%;
-	`;
 
-const TopText = styled.div`
-`;
-
-const TopName = styled.div`
-`;
-
-const TopEmail = styled.div`
-`;
-
-const CardBtm = styled.div`
-`;
-
-const BtmText = styled.div`
-`;
 
 
 
